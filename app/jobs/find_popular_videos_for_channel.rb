c9b1007cad198e4bfbba355ab7421ebd92e01a58
Alex Marchenko <AlexTheProgrammerMail@gmail.com>
2021-02-21 23:32:48 -0800

    fixing finding popular videos

--- app/jobs/find_popular_videos_for_channel.rb
+++ app/jobs/find_popular_videos_for_channel.rb
@@ -7,19 +7,28 @@ class FindPopularVideosForChannel
         driver = Selenium::WebDriver.for :firefox
         driver.navigate.to channel.url
 
-        # go to popular uploads
-        driver.find_element(:xpath, '//span[text()="Popular uploads"]').click
+        # if threre's no channel URL, scrape one
+        if channel.logo_url.blank?
+            binding.pry
+            channel.update!(logo_url: driver.find_element(:css, '#avatar img[width="80"]')[:src])
+        end
+        
+        # go videos
+        driver.find_elements(:css, 'paper-tab[role="tab"]').select{|tab| tab.text == 'VIDEOS'}.first.click
+        driver.find_element(:css, 'paper-button[aria-label="Sort by"]').click
+        sleep(2)
+        driver.find_elements(:css, 'paper-listbox a').select{|item| item.text == 'Most popular'}.first.click
 
         scroll = 0
         videos = nil
         last_videos_count = nil
-        until videos&.count&.>=(1000) do
+        until videos&.count&.>=(500) do
             sleep(5) # todo fix that only 90 vides make it to the DB due to scrollign 
-            driver.execute_script("window.scrollTo(#{scroll}, #{scroll + 1000})")
+            driver.execute_script("window.scrollTo(#{scroll}, #{scroll + 4000})")
             videos = driver.find_elements(:id, 'video-title')
             break if last_videos_count == videos.count
             last_videos_count = videos.count
-            scroll = scroll + 1000
+            scroll = scroll + 4000
         end        
         
         driver.find_elements(:id, 'video-title').each do |video_link|
