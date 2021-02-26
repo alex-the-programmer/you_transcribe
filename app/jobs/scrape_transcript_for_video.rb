b56e3070c0552fd9bd6e54d0875fb6720be4577d
Alex Marchenko <AlexTheProgrammerMail@gmail.com>
2021-02-25 21:35:12 -0800

    switchied video scraper to chrome driver

--- app/jobs/scrape_transcript_for_video.rb
+++ app/jobs/scrape_transcript_for_video.rb
@@ -4,21 +4,27 @@ class ScrapeTranscriptForVideo
     def perform(video_id)
         video = Video.find(video_id)
 
-        driver = Selenium::WebDriver.for :firefox
-        driver.navigate.to "https://www.youtube.com/watch?v=#{video.youtube_id}"
-        sleep(5)
-        driver.find_element(:css, 'button[aria-label="More actions"]').click
+        # @@options ||= begin 
+        #     options = Selenium::WebDriver::Chrome::Options.new
+        #     options.add_argument('--headless')
+        #     options
+        # end
+        @@driver ||= Selenium::WebDriver.for :chrome #, options: @@options
+        @@driver.navigate.to "https://www.youtube.com/watch?v=#{video.youtube_id}"
+        # sleep(2)
+        @@wait ||= Selenium::WebDriver::Wait.new(:timeout => 15)
+        @@wait.until { @@driver.find_element(:css, 'button[aria-label="More actions"]') }.click
         open_transcript_link = nil
         begin
-            open_transcript_link = driver.find_element(:xpath, '//yt-formatted-string[text()="Open transcript"]')
+            open_transcript_link = @@driver.find_element(:xpath, '//yt-formatted-string[text()="Open transcript"]')
         rescue => exception
             video.update!(status: :no_transcript)
         end 
         if open_transcript_link
             open_transcript_link.click
-            sleep(5)
+            # sleep(5)
 
-            transcript = driver.find_elements(:css, '.cue-group').map do |transcript_line|
+            transcript = @@wait.until { @@driver.find_elements(:css, '.cue-group') }.map do |transcript_line|
                 {
                     offset: transcript_line.find_element(:css, '.cue-group-start-offset').text,
                     text: transcript_line.find_element(:css, '.cues .cue').text
@@ -28,7 +34,5 @@ class ScrapeTranscriptForVideo
         else
             video.update!(status: :no_transcript)
         end
-
-        driver.quit
     end
 end
