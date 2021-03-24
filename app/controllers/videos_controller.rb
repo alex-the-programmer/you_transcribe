39d76b8c75e4d6d77f7660227bd20e10601f2a55
Alex Marchenko <AlexTheProgrammerMail@gmail.com>
2021-03-23 22:21:26 -0700

    filter added

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -2,6 +2,7 @@ class VideosController < ApplicationController
     def show
         @video = Video
             .includes(:channel)
+            .transcription_scraped
             .where(channels: { slug: params[:channel_id]})
             .find_by!(slug: params[:id])
     end
